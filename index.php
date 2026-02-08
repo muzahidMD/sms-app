d0c0a2bcb9349e4e0e2114f5f10c5d1f9b5af1ee
muzahidmd <mujahidul.core@gmail.com>
2026-02-08 23:12:26 +0600

    add card

--- index.php
+++ index.php
@@ -61,17 +61,18 @@
     <!-- Teacher Section -->
     <section class="container">
         <h1 class="section-heading">Our Teacher</h1>
-        <div class="card" style="width: 18rem;">
-
-            <img src="./Applications/XAMPP/htdocs/school_management/assets/images/t-1.jpg" class="card-img-top"
-                alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
+        <?php
+        for ($i = 0; $i < 8; $i++) { ?>
+            <div class="card" style="width: 18rem;">
+                <img src="/school_management/assets/images/t-1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="card-button">Details</a>
+                </div>
+                <?php } ?>
         </div>
     </section>
 
