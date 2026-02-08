852b5a8fb90416e1b029e9062aa49f2b6bf83230
muzahidmd <mujahidul.core@gmail.com>
2026-02-08 18:31:34 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar">
+        <div class="navbar-container">
+            <a href="" class="navbar-logo">Online School</a>
+            <button class="navbar-toggle">
+                <spam class="bar"></spam>
+                <spam class="bar"></spam>
+                <spam class="bar"></spam>
+
+                <!-- <i class="fa-solid fa-bars bar"></i><br>
+                <i class="fa-solid fa-x"></i> -->
+            </button>
+            <ul class="navbar-menu">
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#"><i class="fa-solid fa-right-from-bracket"></i></a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Banner section  -->
+    <section class="container banner flex">
+        <div class="half-width flex">
+            <div>
+                <div>
+                    <p class="heading">School Management <br><span>System</span></p>
+                </div>
+                <p class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat quasi quam
+                    perspiciatis, maxime et
+                    sint
+                    qui molestiae est totam similique repellat cupiditate aliquid voluptatem aspernatur, accusamus
+                    praesentium quia necessitatibus officiis!</p>
+                <button class="button">Go To Class</button>
+            </div>
+        </div>
+        <div class="half-width ">
+            <form action="" class="form">
+                <h2>Admission Form</h2>
+                <input type="text" name="" id="" placeholder="Name" class="input">
+                <input type="email" name="" id="" placeholder="Email" class="input">
+                <input type="phone" name="" id="" placeholder="Phone" class="input">
+                <input type="text" name="" id="" placeholder="Class" class="input">
+                <button type="submit" class="button">Submit</button>
+            </form>
+        </div>
+    </section>
+
+    <!-- Teacher Section -->
+    <section class="container">
+        <h1 class="section-heading">Our Teacher</h1>
+        <div class="card" style="width: 18rem;">
+
+            <img src="./Applications/XAMPP/htdocs/school_management/assets/images/t-1.jpg" class="card-img-top"
+                alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's
+                    content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://kit.fontawesome.com/b4ce6053fa.js" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
